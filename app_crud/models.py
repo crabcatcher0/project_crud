f8b97fc8a8153927ee41bb8638be1fe3a08c23d8
crabcatcher0 <rautprashant103@gmail.com>
2024-01-09 15:41:07 +0545

    styling done

--- app_crud/models.py
+++ app_crud/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class Notice(models.Model):
     title = models.CharField(max_length = 300)
     description = models.CharField(max_length = 600)
+    name = models.CharField(max_length = 50, default = 'TJMC')
